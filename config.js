7cbe857be1087a9b8337e6a4e8ebcda60bacf7b9
Ulises <ulisesgascongonzalez@gmail.com>
2016-07-15 13:13:03 +0200

    Dependencies and setup for v.0.0.2

--- config.js
+++ config.js
@@ -1,8 +1,45 @@
 var pjson = require('./package.json');
 
 var config = {
-    version: pjson.version,
-    sheetCode: "", 
+  	version: pjson.version,
+  	creador: pjson.author,
+    sheetCode: "",
+    sheetCodeExclusives: "",
+    // Habgouts BOT
+    hangouts: {
+      usuarioId: "",
+    	usuarioAutorizado: "",
+    	botEmail: "",
+    	botPassword: ""
+    },
+    // Slack BOT
+    slack: {
+      enable: true,
+      keyUser: "",
+      webhookUri: ""
+    },
+    //Twitter
+    twitter : {
+      consumer_key: "", 
+      consumer_secret: "",
+      access_token_key: "",
+      access_token_secret: ""
+    },
+    //FIREBASE SETUP
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    storageBucket: "",
+    type: "",
+    project_id: "",
+    private_key_id: "",
+    private_key: "",
+    client_email: "",
+    client_id: "",
+    auth_uri: "",
+    token_uri: "",
+    auth_provider_x509_cert_url: "",
+    client_x509_cert_url: ""
 };
 
 module.exports = config;
